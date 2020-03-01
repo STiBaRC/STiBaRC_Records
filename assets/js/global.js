c1b57ab06a1023fd302bf31cd1ec06e82215d025
Ben Border <ben@bunnbuns.net>
2020-03-01 02:28:31 -0500

    added mobile nav button

--- assets/js/global.js
+++ assets/js/global.js
@@ -6,6 +6,21 @@ function $(id){  //less typing
     }
 }
 
+/* nav mobile menu & button */
+document.addEventListener("click", function(event) {
+    var isClickInside = $('mobile-btn').contains(event.target);
+    var navLinks = $('nav-links').contains(event.target);
+    
+    if($("nav-links").style.display == "none" || $("nav-links").style.display == ""  || navLinks){
+        $('nav-links').style.display = "block";
+    }else{
+        $("nav-links").style.display = "none";
+    }
+    if (!isClickInside && !navLinks) {
+        //the click was outside the nav dropdown
+        $("nav-links").style.display = "none";
+    }
+});
 // set devMode to false in production //
 var devMode = false;
 
