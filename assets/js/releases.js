2b40422640f9d6864266d5d504e21e5bf77e8806
Ben Border <ben@bunnbuns.net>
2020-03-10 16:02:06 -0400

    changed layout of releases in list mode

--- assets/js/releases.js
+++ assets/js/releases.js
@@ -13,7 +13,7 @@ function release(id, item){
         var artwork = artworkPath + item['artwork']['500px'].replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
         var year = item['year'].replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
         
-        releaseList += '<a class="album album-width" href="#"> <img class="artwork" src="'+artwork+'" title="'+name +' - '+artists+'"> <div class="name">'+name+' - '+artists+' <div class="year">'+year+'</div> </div> </a>';
+        releaseList += '<a class="album album-width" href="#"> <img class="artwork" src="'+artwork+'" title="'+name +' - '+artists+'"> <div class="name">'+name+'<span class="name-spacer"> - </span><span class="name-artists">'+artists+'</span> <div class="year">'+year+'</div> </div> </a>';
     }catch (err){
         console.log(err);
     }
