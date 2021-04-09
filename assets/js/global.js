b3e726c6924807c1c04e6f3bbd1958627f9630b5
Ben Border <ben@bunnbuns.net>
2021-04-09 16:17:28 -0400

    working mobile nav! OwO

--- assets/js/global.js
+++ assets/js/global.js
@@ -19,3 +19,19 @@ function scrollFunction() {
 		$("nav").classList.remove('nav-bg');
 	}
 }
+
+function mobileNav(action) {
+	if (action == "open") {
+		$("openMobileNav").style.display = "none";
+		$("closeMobileNav").style.display = "inline-flex";
+		$("mobileNav").style.display = "block";
+		document.getElementsByTagName("body")[0].style.maxHeight = "100vh";
+		document.getElementsByTagName("body")[0].style.overflow = "hidden";
+	} else {
+		$("openMobileNav").style.display = "inline-flex";
+		$("closeMobileNav").style.display = "none";
+		$("mobileNav").style.display = "none";
+		document.getElementsByTagName("body")[0].style.maxHeight = "auto";
+		document.getElementsByTagName("body")[0].style.overflow = "";
+	}
+}
