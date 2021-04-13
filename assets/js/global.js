5a66a7518dfe4fb6286f2cfe5c5028694b845a5d
Ben Border <ben@bunnbuns.net>
2021-04-13 14:40:43 -0400

    move stuff around. create about page

--- assets/js/global.js
+++ assets/js/global.js
@@ -6,20 +6,6 @@ function $(id) {
 	}
 }
 
-window.onload = scrollFunction();
-
-window.onscroll = function () {
-	scrollFunction();
-};
-
-function scrollFunction() {
-	if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
-		$("nav").classList.add('nav-bg');
-	} else {
-		$("nav").classList.remove('nav-bg');
-	}
-}
-
 function mobileNav(action) {
 	if (action == "open") {
 		$("openMobileNav").style.display = "none";
